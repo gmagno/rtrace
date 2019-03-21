a1e1549e29ed6492ed96e2d3378b9da0ba40b775
Goncalo <goncalo.magno@gmail.com>
2019-03-20 23:33:35 -0300

    Add tests for ray_x_plane, ray_x_polygon and ray_x_sphere

--- test/test_rtrace.py
+++ test/test_rtrace.py
@@ -2,7 +2,9 @@
 import numpy as np
 import pytest
 
-from rtrace.rtrace import ray_x_plane, point_in_polygon
+from rtrace.rtrace import (
+    point_in_polygon, ray_x_plane, ray_x_polygon, ray_x_sphere
+)
 
 @pytest.mark.parametrize('p', [
     {
@@ -38,6 +40,50 @@ def test_ray_x_plane(request, p):
         ]),
         'assert': True
     },
+    {
+        'ri': np.array([2.0, 0.0, 0.0]), 'pn': np.array([-1.0, 1.0, 0.0]),
+        'poly_verts': np.array([
+            [ 2.0, -1.0, -1.0 ], [ 2.0, -1.0,  1.0 ],
+            [ 2.0,  1.0,  1.0 ], [ 2.0,  1.0, -1.0 ],
+        ]),
+        'assert': True
+    },
 ])
 def test_point_in_poly(request, p):
     assert point_in_polygon(p['ri'], p['poly_verts'], p['pn']) == p['assert']
+
+
+@pytest.mark.parametrize('p', [
+    {
+        'r0': np.array([ 1.0, 0.0, 0.0 ]),
+        'rd': np.array([ 1.0, 0.0, 0.0 ]),
+        'poly_verts': np.array([
+            [ 2.0, -1.0, -1.0 ], [ 2.0, -1.0,  1.0 ],
+            [ 2.0,  1.0,  1.0 ], [ 2.0,  1.0, -1.0 ],
+        ]),
+        'pn': np.array([-1.0, 0.0, 0.0 ]),
+        'expected': (np.array([2.0, 0.0, 0.0]), True)
+    },
+])
+def test_ray_x_poly(request, p):
+    ri, is_inside =  ray_x_polygon(
+        p['r0'], p['rd'], p['poly_verts'], p['pn'], tol=1e-6
+    )
+    assert np.allclose(ri, p['expected'][0])
+    assert is_inside == p['expected'][1]
+
+
+@pytest.mark.parametrize('p', [
+    {
+        'r0': np.array([ 0.0, 0.0, 0.0 ]),
+        'rd': np.array([ 1.0, 2.0, 0.0 ]) / np.linalg.norm([1.0, 2.0, 0.0]),
+        'sc': np.array([ 2.0, 2.0, 0.0 ]),
+        'sr': 1.0,
+        'expected': (np.array([1.0, 2.0, 0.0]), True)
+    },
+])
+def test_ray_x_sphere(request, p):
+    ri, hit_sphere = ray_x_sphere(p['r0'], p['rd'], p['sc'], p['sr'], tol=1e-6)
+    assert np.allclose(ri, p['expected'][0])
+    assert hit_sphere == p['expected'][1]
+
